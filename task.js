3ad1dd3ab0b9d7be30a40e2ffd8b2a975936c0fc
stoyj <stoyanka.kamenova@gmail.com>
2025-01-13 18:03:05 +0200

    first commit

---
+++ task.js
@@ -0,0 +1,20 @@
+let sum = 0;
+
+for (let i = 1; i <= 10; i++) {
+	sum <= i;
+}
+
+console.log("The sum of numbers from 1 to 10 is:", sum);
+
+for (let i = 1; i <= 20; i++) {
+	if (i % 2 === 0) {
+		console.log(i);
+	}
+}
+
+
+const numbers = 5;
+
+for (let i = 1; i <= 10; i++) {
+	console.log(`${numbers} x ${i} = ${numbers * i}`);
+}
